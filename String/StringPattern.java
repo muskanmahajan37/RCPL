423f148772e44ac5031d57ba73bafb5187cac8f2
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-05-31 04:34:52 +0530

    pattern

--- String/StringPattern.java
+++ String/StringPattern.java
@@ -9,11 +9,22 @@ class StringPattern{
 		System.out.print("Enter string - ");
 		str=sc.next();
 		str=str.toUpperCase();
+		System.out.println("Pattern 1 - ");
 		for(int i=0;i<str.length();i++)
 		{
 			for(int j=0;j<str.length()-i;j++)
 			{
 				System.out.print(str.charAt(j));
+			}
+			System.out.println();
+		}
+		System.out.println("Pattern 2 -");
+		for(int i=0;i<str.length();i++)
+		{	
+			char a=str.charAt(i);
+			for(int j=0;j<str.length()-i;j++)
+			{
+				System.out.print(a);
 			}
 			System.out.println();
 		}
