3a9616010caaf965759e43eb77573c213dedf9a4
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-07-16 15:48:09 +0530

    ques 2

---
+++ Day 10/Marksheet.java
@@ -0,0 +1,14 @@
+class Marksheet{
+	public static void main(String[] args) {
+		System.out.println("   Marksheet   ");
+		System.out.println("......................");
+		System.out.println("Name : Rahul Jain \nRoll No  :  0205CS071101\nCollege  :  IIIT Allahabad.");
+		System.out.println("......................");
+		System.out.println("Subject   :    Marks");
+		System.out.println("Sub1 : 70 \nSub2 : 80\nSub3 : 80\nSub4 : 50\nSub5 : 69");
+		System.out.println("......................");
+		System.out.println("Total : 349\nPercent : 69.80\nDivision : First");
+		System.out.println("......................");
+	}
+
+}
