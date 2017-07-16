412667b753d94668a003140113ebe961da507ff4
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-07-16 15:05:18 +0530

    ques 2

---
+++ Day 2/Pyramid.java
@@ -0,0 +1,18 @@
+public class Pyramid
+{
+public static void main(String[] args)
+{
+int j;
+for(int i = 1; i <= 5; i++) 
+{
+for(j = 1; j <= 6-i; j++) 
+System.out.print(" ");
+for(j = 1; j <= 2*i-1; j++)
+if(j <= i)
+System.out.print((char)(char)(j+64)+""); 
+else
+System.out.print((char)(char)(2*i-j+64)+"");
+System.out.println(); 
+}
+}
+}
